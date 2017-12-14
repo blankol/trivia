7c0997e837c4da4d1cff2e797b4895c0d45e2b95
Annette Coll <annette.coll@outlook.com>
2017-12-13 19:11:18 -0500

    draft of jquery submit

--- script.js
+++ script.js
@@ -1,54 +1,11 @@
 // console.log('Hello Word')
 
-//Create array with questions and answer options. Include correct answer.
-var triviaQuestions = [
- {
-     question: 'Hall and Oates are ...',
-     answers: {
-         a: 'A notorious Wall Street gang.',
-         b: 'A nutritous oatmeal brand that is now banned from 6 states.',
-         c: 'Two white dudes who sing R&B pop rock. ',
-     },
-     correctAnswer:'c'
- },
 
-{
- question: 'Hall and Oates are ...',
- answers: {
-     a: 'A notorious Wall Street gang.',
-     b: 'A nutritous oatmeal brand that is now banned from 6 states.',
-     c: 'Two white dudes who sing R&B pop rock. ',
- },
- correctAnswer:'c'
-},
-
-{
-    question: 'Hall and Oates are ...',
-    answers: {
-        a: 'A notorious Wall Street gang.',
-        b: 'A nutritous oatmeal brand that is now banned from 6 states.',
-        c: 'Two white dudes who sing R&B pop rock. ',
-    },
-    correctAnswer:'b'
-   },
-
-   {
-    question: 'Hall and Oates are ...',
-    answers: {
-        a: 'A notorious Wall Street gang.',
-        b: 'A nutritous oatmeal brand that is now banned from 6 states.',
-        c: 'Two white dudes who sing R&B pop rock. ',
-    },
-    correctAnswer:'c'
-   },
-   {
-    question: 'Hall and Oates are ...',
-    answers: {
-        a: 'A notorious Wall Street gang.',
-        b: 'A nutritous oatmeal brand that is now banned from 6 states.',
-        c: 'Two white dudes who sing R&B pop rock. ',
-    },
-    correctAnswer:'a'
-   },
-         
-]
+    $('form').submit(function (event) {
+        if ($('#submit1').val() === 'C' ) {
+            $( 'span' ).text('Checking...').show();
+            return;
+        }
+        $('span').text('Incorrect!').show().fadeOut(1000);
+        event.preventDefault();
+    });
