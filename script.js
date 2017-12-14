f394bc00a11acdb073f150b510a0264269b23cf0
Annette Coll <annette.coll@outlook.com>
2017-12-14 11:18:30 -0500

    disabled input

--- script.js
+++ script.js
@@ -3,22 +3,28 @@
 $(document).ready(function() {
 
     // Work in progress: 'Processing' text below form to enter after submit and before alert. 
-     $('span').text('Processing......').show().fadeOut(4000); 
-        //  event.preventDefault();
+    //  $('span').text('Processing......').show().fadeOut(4000); 
+    //      event.preventDefault();
 
     // Question function event.
     // Scoring attached to specific question.
-    // Prevent default to stop page from refreshing plus keep score count.
-    // If and else statements for correct and wrong answer. 
-    // Answer can be submitted in either lowercase or uppercase.     
+    // Prevent default to stop page from refreshing plus keep score count.    
     $('#question1').submit(function (event) {
+        $('span').text('Processing......').show().fadeOut(4000); 
+        event.preventDefault();
         event.preventDefault()
         var enter = $('#submit1').html()
         var score = parseInt($('.score').html())
         var points = score + 5 
-        if($('#submit1').val().toUpperCase() === 'C') {
+        
+        // Stop user from re-entering input multiple times
+        $("#submit1").prop('disabled', true) 
+
+        // If and else statements for correct and wrong answer. 
+        // Answer can be submitted in either lowercase or uppercase. 
+        if($('#submit1').val().toUpperCase() === 'C') {   
         parseInt($('.score').html(points))
-            alert('Success!')    
+            alert('Outrageous!')    
         } else {
             alert('Totally Wrong!')
         }
@@ -31,6 +37,8 @@ $(document).ready(function() {
         var enter = $('#submit2').html()
         var score = parseInt($('.score').html())
         var points = score + 5 
+        $("#submit2").prop('disabled', true) 
+        
            if($('#submit2').val().toUpperCase() === 'B' ) {
             parseInt($('.score').html(points))
                alert('OUTRAGEOUS!')    
@@ -45,6 +53,8 @@ $(document).ready(function() {
         var enter = $('#submit2').html()
         var score = parseInt($('.score').html())
         var points = score + 5 
+        $("#submit3").prop('disabled', true) 
+        
         if($('#submit3').val().toUpperCase() === 'C') {
         parseInt($('.score').html(points))        
             alert('OUTRAGEOUS!')    
@@ -59,6 +69,8 @@ $(document).ready(function() {
         var enter = $('#submit2').html()
         var score = parseInt($('.score').html())
         var points = score + 5 
+        $("#submit5").prop('disabled', true) 
+        
         if($('#submit4').val().toUpperCase() === 'B') {
         parseInt($('.score').html(points))     
             alert('OUTRAGEOUS!')    
@@ -73,6 +85,8 @@ $(document).ready(function() {
         var enter = $('#submit2').html()
         var score = parseInt($('.score').html())
         var points = score + 5 
+        $("#submit5").prop('disabled', true) 
+        
         if($('#submit5').val().toUpperCase() === 'C') {
         parseInt($('.score').html(points))      
             alert('OUTRAGEOUS!')    
@@ -86,4 +100,5 @@ $(document).ready(function() {
 
 
 // Source for jQuery submit form https://api.jquery.com/submit/
+// Source for disabling user from inputing more than once: https://stackoverflow.com/questions/1414365/disable-enable-an-input-with-jquery
 // Sources from PixelArt and ATM Lab.
