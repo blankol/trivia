91865ff96cb27ea285133505c487b20593538827
Annette Coll <annette.coll@outlook.com>
2017-12-14 00:34:48 -0500

    corrected jquery + added css/html

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // console.log('Hello Word')
 
 $(document).ready(function() {
-     $('span').text('Validating......').show().fadeOut(4000);
+     $('span').text('Processing......').show().fadeOut(4000);
          event.preventDefault();
 
     $('#question1').submit(function (event) {
@@ -14,13 +14,40 @@ $(document).ready(function() {
     })
     
        $('#question2').submit(function (event) {
-           if($('#submit2').val() === 'b') {
+           if($('#submit2').val() === 'B') {
                alert('Success!')    
            } else {
                alert('Totally Wrong!')
            }
          
        })
+
+       $('#question3').submit(function (event) {
+        if($('#submit3').val() === 'b') {
+            alert('Success!')    
+        } else {
+            alert('Totally Wrong!')
+        }
+      
+    })
+
+    $('#question4').submit(function (event) {
+        if($('#submit4').val() === 'b') {
+            alert('Success!')    
+        } else {
+            alert('Totally Wrong!')
+        }
+      
+    })
+
+    $('#question5').submit(function (event) {
+        if($('#submit5').val() === 'b') {
+            alert('Success!')    
+        } else {
+            alert('Totally Wrong!')
+        }
+      
+    })
     
     })
 
